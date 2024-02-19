4c1305ccdaaf701426a5a6f2f47bab32d6b26e04
Pablobi <pablo.bermudez.medina@gmail.com>
2024-02-20 00:05:19 +0100

    D02-2, D02-3, D02-4, D02-5 ,D02-13  done

---
+++ src/main/java/acme/entities/TrainingModule.java
@@ -0,0 +1,59 @@
+
+package acme.entities;
+
+import java.sql.Time;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingModule extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	private String				code;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				creationMoment;
+
+	@NotBlank
+	@Length(max = 101)
+	private String				details;
+
+	@NotNull
+	private DifficultyLevel		difficultyLevel;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	private Date				updateMoment;
+
+	@URL
+	private String				link;
+
+	private Time				totalTime;
+
+	// Relationships ----------------------------------------------------------
+
+}
---
+++ src/main/java/acme/entities/TrainingSession.java
@@ -0,0 +1,54 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingSession extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	private String				code;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				location;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				instructor;
+
+	@NotBlank
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private TrainingModule		trainingModule;
+
+}
