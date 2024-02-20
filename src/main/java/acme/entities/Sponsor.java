43070d18921156268d45ebfb2a18d8c0b0ac3cb8
Alexander Picón Garrote <alepingar@alum.us.es>
2024-02-20 18:41:36 +0100

    Sponsor class actualizada

--- src/main/java/acme/entities/Sponsor.java
+++ src/main/java/acme/entities/Sponsor.java
@@ -1,6 +1,71 @@
+
 package acme.entities;
 
+import java.util.List;
 
-public class Sponsor {
+import javax.persistence.Entity;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Sponsor extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	private List<Invoice>		invoices;
+
+	private List<Sponsorship>	sponsorships;
+
+
+	public int invoicesTaxed() {
+		return (int) this.invoices.stream().filter(i -> i.getTax() <= 21.00).count();
+	}
+
+	public int getTotalSponsorshipsWithLink() {
+		return (int) this.sponsorships.stream().filter(sponsorship -> sponsorship.getOptionalLink() != null && !sponsorship.getOptionalLink().isEmpty()).count();
+	}
+
+	public double getAverageAmountOfSponsorships() {
+		return this.sponsorships.stream().mapToDouble(s -> s.getAmount()).average().orElse(0);
+	}
+
+	public double getStandardDeviationOfSponsorshipAmounts() {
+		double average = this.getAverageAmountOfSponsorships();
+		double variance = this.sponsorships.stream().mapToDouble(sponsorship -> Math.pow(sponsorship.getAmount() - average, 2)).average().orElse(0.0);
+		return Math.sqrt(variance);
+	}
+
+	public double getMinimumAmountOfSponsorships() {
+		return this.sponsorships.stream().mapToDouble(Sponsorship::getAmount).min().orElse(0.0);
+	}
+
+	public double getMaximumAmountOfSponsorships() {
+		return this.sponsorships.stream().mapToDouble(Sponsorship::getAmount).max().orElse(0.0);
+	}
+
+	public double getAverageQuantityOfInvoices() {
+		return this.invoices.stream().mapToDouble(Invoice::getQuantity).average().orElse(0.0);
+	}
+
+	public double getStandardDeviationOfInvoiceQuantities() {
+		double average = this.getAverageQuantityOfInvoices();
+		double variance = this.invoices.stream().mapToDouble(invoice -> Math.pow(invoice.getQuantity() - average, 2)).average().orElse(0.0);
+		return Math.sqrt(variance);
+	}
+
+	public int getMinimumQuantityOfInvoices() {
+		return this.invoices.stream().mapToInt(Invoice::getQuantity).min().orElse(0);
+	}
+
+	public int getMaximumQuantityOfInvoices() {
+		return this.invoices.stream().mapToInt(Invoice::getQuantity).max().orElse(0);
+	}
 
 }
