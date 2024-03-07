c64a1fae4aa01db6feb653295848d6bf15be08a8
DaniFdezCab <danfercab@alum.us.es>
2024-03-07 18:14:05 +0100

    Suplementary Requirements D02

--- src/main/java/acme/entities/projects/UserStory.java
+++ src/main/java/acme/entities/projects/UserStory.java
@@ -3,6 +3,7 @@ package acme.entities.projects;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
@@ -49,9 +50,11 @@ public class UserStory extends AbstractEntity {
 	private String				url;
 
 	@ManyToOne(optional = false)
+	@Valid
 	private Project				project;
 
 	@ManyToOne(optional = false)
+	@Valid
 	private Manager				manager;
 
 }
