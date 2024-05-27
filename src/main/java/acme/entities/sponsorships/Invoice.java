a28bdd4560bd8161149fc93a4936e85607b0bcf5
Alexander Picón Garrote <alepingar@alum.us.es>
2024-05-27 21:51:08 +0200

    Revert "feat: indexes"
    This reverts commit b7832e6fa4f1ef04a9c4616c1e678c6094058d93.

--- src/main/java/acme/entities/sponsorships/Invoice.java
+++ src/main/java/acme/entities/sponsorships/Invoice.java
@@ -6,9 +6,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -30,9 +28,6 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(indexes = {
-	@Index(columnList = "id"), @Index(columnList = "sponsorship_id"), @Index(columnList = "code")
-})
 public class Invoice extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
